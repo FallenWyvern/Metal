ffbd0fdecc273d1441cd01884df2ce3ddcf74ff7
FallenWyvern <b.holmes.29@gmail.com>
2015-09-25 19:55:51 -0400

    Initial Publish
    Initial Publish

---
+++ New Unity Project/Assets/LightFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFollow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.mousePosition.ToString ());
+
+		float mouseX = 0;		
+		mouseX = 4 * ((Input.mousePosition.x - Screen.width ) / (Screen.width / 2));
+
+		float mouseY = 0;		
+		mouseY = 4 * ((Input.mousePosition.y - Screen.height / 2) / (Screen.height / 2));
+
+		this.transform.position = new Vector3 ((float)mouseX, (float)mouseY, -2.78f);
+	}
+}
